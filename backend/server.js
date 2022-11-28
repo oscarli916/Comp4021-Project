51fed7b79abfbd27aebd766a6ec31755292c639a
Oscar Li <oscar_li916@yahoo.com.hk>
2022-11-29 00:43:19 +0800

    feat: backend ranking

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const fs = require("fs");
 
 const USER_FILE_PATH = "backend/data/users.json";
+const RANKING_PATH = "backend/data/ranking.json";
 
 const app = express();
 app.use(express.static("frontend"));
@@ -69,6 +70,39 @@ app.post("/signin", (req, res) => {
     message: "successfully logged in",
     user: { name: users[username]["name"], username: username },
   });
+});
+
+app.get("/ranking", (req, res) => {
+  const ranking = JSON.parse(fs.readFileSync(RANKING_PATH));
+
+  res.json({
+    status: "success",
+    message: "successfully get ranking",
+    data: ranking,
+  });
+});
+
+app.post("/ranking", (req, res) => {
+  const { username, score } = req.body;
+
+  const ranking = JSON.parse(fs.readFileSync(RANKING_PATH));
+
+  let minScore = ranking[0].score;
+  let minScoreIdx = 0;
+  for (let i = 0; i < ranking.length; i++) {
+    if (ranking[i].score < minScore) {
+      minScore = ranking[i].score;
+      minScoreIdx = i;
+    }
+  }
+
+  if (minScore < score) {
+    ranking[minScoreIdx] = { username: username, score: score };
+    fs.writeFileSync(RANKING_PATH, JSON.stringify(ranking, null, " "));
+    res.json({ status: "success", message: "ranking is updated" });
+    return;
+  }
+  res.json({ status: "success", message: "ranking remain unchanged" });
 });
 
 app.listen(8000, () => {
